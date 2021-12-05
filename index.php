d3d77e113196c664a08cc7a38473769c4a33cb01
Aleksandr Kasko-Spak <webmaster@papagai.de>
2021-12-05 19:40:12 +0100

    creation seven php blocks,

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 <?php include("./php-variable/activities.php")?>
 <?php include("./php-variable/footer.php")?>
 <?php include("./php-variable/modal.php")?>
-
     <script src="js/jquery-3.6.0.min.js"></script>
     <script src="js/swiper-bundle.min.js"></script>
     <script src="js/main.js"></script>
